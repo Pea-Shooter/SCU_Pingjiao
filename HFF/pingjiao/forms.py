cc52c674c4be5099589abe53605cb9d852c5e492
Kornberg <kornbergfresnel@outlook.com>
2016-12-24 19:25:02 +0800

    fix widgets' bugs

--- HFF/pingjiao/forms.py
+++ HFF/pingjiao/forms.py
@@ -11,11 +11,11 @@ class PostUserData(forms.Form):
 
     stu_id = forms.CharField(required=True, label='Student ID', error_messages={
         'stu_id_check': "Please enter the correct student id, there must be 11 digits"
-    }, widget=widgets.TextInput(attrs={'placeholder': 'Enter your student id at here', 'class': 'form-ele'}))
+    }, widget=widgets.TextInput(attrs={'placeholder': 'Enter your student id at here', 'class': 'form-control input-lg'}))
 
     passwd = forms.CharField(required=True, label='Password', error_messages={
         'passwd_check': "Please enter the correct password which attaches to your id"
-    }, widget=widgets.PasswordInput(attrs={'placeholder': 'Enter your password at here', 'class': 'form-ele'}))
+    }, widget=widgets.PasswordInput(attrs={'placeholder': 'Enter your password at here', 'class': 'form-control input-lg'}))
 
     def clean(self):
         """Clean-function will handle some error, and raise error_message to templates
@@ -30,12 +30,12 @@ class PostUserData(forms.Form):
         stu_id = cleaned_data.get('stu_id')
         passwd = cleaned_data.get('passwd')
 
-        if stu_id == '':
+        if stu_id is None:
             raise forms.ValidationError("Student ID is required!")
-        if passwd == '':
+        if passwd is None:
             raise forms.ValidationError("Password is required!")
         if self.check_format(stu_id):
-            raise forms.ValidationError(self.errors['stu_id_check'])
+            raise forms.ValidationError("Password is required!")
 
     def check_format(self, stu_id):
         try:
