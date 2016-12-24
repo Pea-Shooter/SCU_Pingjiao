761d317b58d857a4a1d531ef69a605febe129f89
Kornberg <kornbergfresnel@outlook.com>
2016-12-24 15:49:14 +0800

    add pj-view

--- HFF/pingjiao/views.py
+++ HFF/pingjiao/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import View
+from .forms import PostUserData
+
 
 # Create your views here.
+class PJView(View):
+    """Show the pingjiao's main page
+    """
+
+    template_name = 'pingjiao/mainpage.html'
+    pj_form_name = 'pj_form'
+
+    def get(self, request, *args, **kwargs):
+        """At this function, handle the get-method request
+
+        Data: load a form in context
+        """
+        form = PostUserData()
+        context = {self.pj_form_name: form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        """At this function, handle the post-method request
+
+        Valid the students' id and their password
+        """
+        form = PostUserData(resquest.POST)
+        if form.is_valid():
+            # do pingjiao action, and display the processing in the template
+            return HttpResponse('Success!')
+        else:
+            context = {self.pj_form_name: form}
+            return render(request, self.template_name, context)
