f408a0c5798d784b799ac5cbb85141070e3f97c2
Kornberg <kornbergfresnel@outlook.com>
2016-12-25 16:17:47 +0800

    add custom errors

--- HFF/pingjiao/forms.py
+++ HFF/pingjiao/forms.py
@@ -10,11 +10,12 @@ class PostUserData(forms.Form):
     """
 
     stu_id = forms.CharField(required=True, label='Student ID', error_messages={
-        'stu_id_check': "Please enter the correct student id, there must be 11 digits"
+        'required': 'Student ID is required!',
+        'id_check': 'Student ID must be 11 digits!'
     }, widget=widgets.TextInput(attrs={'placeholder': 'Enter your student id at here', 'class': 'form-control input-lg'}))
 
     passwd = forms.CharField(required=True, label='Password', error_messages={
-        'passwd_check': "Please enter the correct password which attaches to your id"
+        'required': 'Passwdord is required!'
     }, widget=widgets.PasswordInput(attrs={'placeholder': 'Enter your password at here', 'class': 'form-control input-lg'}))
 
     def clean(self):
@@ -30,12 +31,9 @@ class PostUserData(forms.Form):
         stu_id = cleaned_data.get('stu_id')
         passwd = cleaned_data.get('passwd')
 
-        if stu_id is None:
-            raise forms.ValidationError("Student ID is required!")
-        if passwd is None:
-            raise forms.ValidationError("Password is required!")
-        if self.check_format(stu_id):
-            raise forms.ValidationError("Password is required!")
+        if stu_id and self.check_format(stu_id) is False:
+            temp = self.fields['stu_id']
+            raise forms.ValidationError(self.fields['stu_id'].error_messages['id_check'], code='stu_id')
 
     def check_format(self, stu_id):
         try:
